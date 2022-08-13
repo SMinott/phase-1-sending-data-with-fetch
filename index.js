14dd97bd65bf111783b9d29d0cfb2730682f1551
Susan Minott <susan_minott@yahoo.com>
2022-08-13 18:52:30 -0400

    commit tree 3b2559dc9d21496609706ed934020b35e774d94f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData() {
+    fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json',
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+        })
+    })
+    .then(resp => resp.json)
+    .then(data => submitData(data))
+    .catch(error => alert('DANGER!!!'))
+    // .catch(error => alert(error.message))
+
+    return fetch('http://localhost:3000/users')
+}
+
+// function fetchData(){
+// fetch('http://localhost:3000/users', submitData)
+// .then(resp => resp.json)
+// .then(data => submitData(data))
+// .catch(error => alert('DANGER!!!'))
+// }
